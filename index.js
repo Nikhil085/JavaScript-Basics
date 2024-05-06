5af80204b0d692d03d63369aa3f1b09b7246e18c
Nikhil <56070080+Nikhil085@users.noreply.github.com>
2024-05-06 18:01:07 +0000

    Let Const Var Practice

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Practice Start Now !")
+const accoutn = 1565;
+let accountEmail = "Nikhil@123.gail.com";
+var accoutnPassword = "12345";
+accoutnCity = "Jaipur";
+
+// we are not override constant value 
+//block scope 
+// accoutn = "23123";
+// console.log(accoutn);_
+
+
+// // let  we can Override / block scope
+// accountEmail = "NikhilMahaan929@hmail.com";
+// console.log(accountEmail);
+
+// var is use global scope
+// var also we override ;
+
