d2f06310af6bfa5f474705e976013add8b4456f8
Nikhil <56070080+Nikhil085@users.noreply.github.com>
2024-05-06 18:16:39 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+// javaScript dataTypes
+/*
+
+String ,Boolean, Null , Undefine, Number, RegExp, Symbol ,bigint, object ;
+
+*/
+
+let data;
+
+console.log(typeof("Nikhil"));
+console.log(typeof(231));
+console.log(typeof(true));
+console.log(data);
+console.log(typeof(BigInt(2387408709857324587)));
+console.log("");
+console.log(typeof({}));
+console.log(null);
+
+
+
+
