1099af4514c1df40fc5a6f9f5de27c9864d885ea
Nikhil <56070080+Nikhil085@users.noreply.github.com>
2024-05-07 17:57:02 +0000

    Conversion inn js

---
+++ 03_typeconverstion.js
@@ -0,0 +1,44 @@
+// type Checked 
+// let data = 22;
+// console.log(typeof data);
+
+// convert type using String literals (" "); 
+
+// let data = "22";
+// console.log(typeof data);
+
+// given NaN
+
+// let data = "23dfsd";
+// let num = Number(data);
+// console.log(num);
+
+// given NUll At that time 
+
+ let data = null;
+ let num = Number(data);
+ console.log(num);
+
+ // and we try to change String to the num its given Nan ;
+  
+
+ // 0 = false and 1 = true
+ let log = 1;
+ let con = Boolean(log);
+ console.log(log);
+
+ //empty STring given false
+let str = "";
+let m = Boolean(str);
+console.log(m);
+
+// given 0
+// let arr = [];
+// let arr1 = Number(arr);
+// console.log(arr1);
+
+//  Given NaN
+let arr = {};
+let arr1 = Number(arr);
+console.log(arr1);
+
