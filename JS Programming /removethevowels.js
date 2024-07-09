11bfbe35f149679695c1e8bfd1a9e826fe3101d6
Nikhil <56070080+Nikhil085@users.noreply.github.com>
2024-07-09 17:08:28 +0000

    practice

---
+++ JS Programming /removethevowels.js
@@ -0,0 +1,16 @@
+// function removeVowels(arr){
+//     let newStr = '';
+//     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+//     for(let i=0; i<arr.length; i++){
+//         if(!vowels.includes(arr[i])){
+//             newStr += arr[i];
+//         }
+//     }
+//     return newStr;
+// }
+
+// let arr = "the Character is Not Show in Array."
+// console.log(removeVowels(arr))
+
+
